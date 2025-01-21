3775e68e35571dd31411869e98791eb6608b4e57
Verbitski Alexandr <verbitski.alexandr@gmail.com>
2025-01-21 14:45:03 +0300

    cookie

--- src/lib/serverActions/auth/index.tsx
+++ src/lib/serverActions/auth/index.tsx
@@ -63,7 +63,7 @@ const loginIntoAccount = async (fd: FormData): Promise<ActionResponse> => {
             cookies().set(TOKENS_KEYS.access, cookieValue, {
                 priority: 'high',
                 sameSite: 'lax',
-                domain: '.aitarot.ai',
+                domain: '.aitarot.io',
                 secure: true,
                 expires: expires,
                 httpOnly: true
@@ -110,7 +110,7 @@ const registerAccount = async (fd: FormData): Promise<ActionResponse> => {
             cookies().set(TOKENS_KEYS.access, cookieValue, {
                 priority: 'high',
                 sameSite: 'lax',
-                domain: '.aitarot.ai',
+                domain: '.aitarot.io',
                 secure: true,
                 expires: expires,
                 httpOnly: true
