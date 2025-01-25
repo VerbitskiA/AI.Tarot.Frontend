f85f601f0304472f55f2e06f6a20c6a16ceebe9d
Verbitski Alexandr <verbitski.alexandr@gmail.com>
2025-01-26 01:14:16 +0300

    Meta Pixel Code

--- src/lib/serverActions/auth/index.tsx
+++ src/lib/serverActions/auth/index.tsx
@@ -6,222 +6,222 @@ import {TOKENS_KEYS} from "@/configs/http-service/constants/authTokens";
 import {cookies} from "next/headers";
 
 
-const getConfiguration = async(): Promise<ConfigurationType> => {
-    const res = await fetchService.get(`api/configuration/`,
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': '*/*',
-            }
-        })
-    return res.data
+const getConfiguration = async (): Promise<ConfigurationType> => {
+	const res = await fetchService.get(`api/configuration/`,
+		{
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': '*/*'
+			}
+		})
+	return res.data
 }
 
 
-const checkEmailExists = async(fd: FormData): Promise<ActionResponse> => {
-    try {
-        const res = await fetchService.get(`api/account/exists/${fd.get('email')}`)
-        if(!!res.data) {
-            const message = res.data?.detail
-            throw new Error(message)
-        }
-    } catch (e) {
-        if (e instanceof Error) {
-            return {
-                status: 'error',
-                message: e.message,
-            }
-        }
-        return {
-            status: 'error',
-            message: 'Что-то пошло не так, попробуйте еще раз',
-        }
-    }
-    return {
-        status: 'ok',
-        message: ''
-    }
+const checkEmailExists = async (fd: FormData): Promise<ActionResponse> => {
+	try {
+		const res = await fetchService.get(`api/account/exists/${fd.get('email')}`)
+		if (!!res.data) {
+			const message = res.data?.detail
+			throw new Error(message)
+		}
+	} catch (e) {
+		if (e instanceof Error) {
+			return {
+				status: 'error',
+				message: e.message
+			}
+		}
+		return {
+			status: 'error',
+			message: 'Что-то пошло не так, попробуйте еще раз'
+		}
+	}
+	return {
+		status: 'ok',
+		message: ''
+	}
 
 }
 
 
 const loginIntoAccount = async (fd: FormData): Promise<ActionResponse> => {
-    try {
-        const res = await fetchService.post('api/account/login/', {
-            body: JSON.stringify({
-                email: fd.get('email'),
-                password: fd.get('password')
-            }),
-        })
+	try {
+		const res = await fetchService.post('api/account/login/', {
+			body: JSON.stringify({
+				email: fd.get('email'),
+				password: fd.get('password')
+			})
+		})
 
-        if (res.ok) {
-            const cookieHeader = res.headers.get('Set-Cookie') || '';  // Provide a default empty string if null
-            const cookie = cookieHeader?.split(';')
-            const cookieValue = cookie[0].split('=')[1];
-            const expiresStr = cookie[1].split('=')[1];
-            const expires = new Date(expiresStr);
-            cookies().set(TOKENS_KEYS.access, cookieValue, {
-                priority: 'high',
-                sameSite: 'lax',
-                domain: '.aitarot.io',
-                secure: true,
-                expires: expires,
-                httpOnly: true
-            });
-        } else {
-            const message = res.data?.detail
-            throw new Error(message)
-        }
-    } catch (e) {
-        if (e instanceof Error) {
-            return {
-                status: 'error',
-                message: e.message,
-            }
-        }
-        return {
-            status: 'error',
-            message: 'Что-то пошло не так, попробуйте еще раз',
-        }
-    }
-    return {
-        status: 'ok',
-        message: 'Аутентификация успешна'
-    }
+		if (res.ok) {
+			const cookieHeader = res.headers.get('Set-Cookie') || '';  // Provide a default empty string if null
+			const cookie = cookieHeader?.split(';')
+			const cookieValue = cookie[0].split('=')[1];
+			const expiresStr = cookie[1].split('=')[1];
+			const expires = new Date(expiresStr);
+			cookies().set(TOKENS_KEYS.access, cookieValue, {
+				priority: 'high',
+				sameSite: 'lax',
+				domain: '.aitarot.io',
+				secure: true,
+				expires: expires,
+				httpOnly: true
+			});
+		} else {
+			const message = res.data?.detail
+			throw new Error(message)
+		}
+	} catch (e) {
+		if (e instanceof Error) {
+			return {
+				status: 'error',
+				message: e.message
+			}
+		}
+		return {
+			status: 'error',
+			message: 'Что-то пошло не так, попробуйте еще раз'
+		}
+	}
+	return {
+		status: 'ok',
+		message: 'Аутентификация успешна'
+	}
 }
 
 const registerAccount = async (fd: FormData): Promise<ActionResponse> => {
-    try {
-        const res = await fetchService.post('api/account/register/', {
-            body: JSON.stringify({
-                username: fd.get('username'),
-                email: fd.get('email'),
-                password: fd.get('password'),
-                dateOfBirth: fd.get('dateOfBirth'),
-                gender: fd.get('gender'),
-            }),
-        })
-        if (res.ok) {
-            const cookieHeader = res.headers.get('Set-Cookie') || '';  // Provide a default empty string if null
-            const cookie = cookieHeader?.split(';')
-            const cookieValue = cookie[0].split('=')[1];
-            const expiresStr = cookie[1].split('=')[1];
-            const expires = new Date(expiresStr);
-            cookies().set(TOKENS_KEYS.access, cookieValue, {
-                priority: 'high',
-                sameSite: 'lax',
-                domain: '.aitarot.io',
-                secure: true,
-                expires: expires,
-                httpOnly: true
-            });
-            console.info('Login successful, tokens have been installed')
-        } else {
-            const message = res.data?.detail
-            throw new Error(message)
-        }
-    } catch (e) {
-        if(e instanceof Error) {
-            return {
-                status: 'error',
-                message: e.message
-            }
-        } else {
-            return {
-                status: 'error',
-                message: 'Не удалось создать аккаунт'
-            }
-        }
-    }
-    return {
-        status: 'ok',
-        message: 'Htubcnhfwbz успешна'
-    }
+	try {
+		const res = await fetchService.post('api/account/register/', {
+			body: JSON.stringify({
+				username: fd.get('username'),
+				email: fd.get('email'),
+				password: fd.get('password'),
+				dateOfBirth: fd.get('dateOfBirth'),
+				gender: fd.get('gender')
+			})
+		})
+		if (res.ok) {
+			const cookieHeader = res.headers.get('Set-Cookie') || '';  // Provide a default empty string if null
+			const cookie = cookieHeader?.split(';')
+			const cookieValue = cookie[0].split('=')[1];
+			const expiresStr = cookie[1].split('=')[1];
+			const expires = new Date(expiresStr);
+			cookies().set(TOKENS_KEYS.access, cookieValue, {
+				priority: 'high',
+				sameSite: 'lax',
+				domain: '.aitarot.io',
+				secure: true,
+				expires: expires,
+				httpOnly: true
+			});
+			console.info('Login successful, tokens have been installed')
+		} else {
+			const message = res.data?.detail
+			throw new Error(message)
+		}
+	} catch (e) {
+		if (e instanceof Error) {
+			return {
+				status: 'error',
+				message: e.message
+			}
+		} else {
+			return {
+				status: 'error',
+				message: 'Не удалось создать аккаунт'
+			}
+		}
+	}
+	return {
+		status: 'ok',
+		message: 'Htubcnhfwbz успешна'
+	}
 }
 
 const successResponse = async (): Promise<ActionResponse> => {
-    return {
-        status: 'ok',
-        message: ''
-    }
+	return {
+		status: 'ok',
+		message: ''
+	}
 }
 
 const resetPassword = async (fd: FormData): Promise<ActionResponse> => {
-    try {
-        const response = await fetchService.post('authentication/reset_password/', {
-            body: JSON.stringify({
-                email: fd.get('email'),
-            })
-        })
-        if(!response.ok) {
-            const message = response.data.detail
-            throw new Error(message)
-        } else {
-            return {
-                status: 'ok',
-                message: 'Письмо отправлено на вашу почту'
-            }
-        }
+	try {
+		const response = await fetchService.post('authentication/reset_password/', {
+			body: JSON.stringify({
+				email: fd.get('email')
+			})
+		})
+		if (!response.ok) {
+			const message = response.data.detail
+			throw new Error(message)
+		} else {
+			return {
+				status: 'ok',
+				message: 'Письмо отправлено на вашу почту'
+			}
+		}
 
-    } catch (e) {
-        if(e instanceof Error) {
-            return {
-                status: 'error',
-                message: e.message
-            }
-        } else {
-            return {
-                status: 'error',
-                message: 'Не удалось отправить письмо'
-            }
-        }
-    }
+	} catch (e) {
+		if (e instanceof Error) {
+			return {
+				status: 'error',
+				message: e.message
+			}
+		} else {
+			return {
+				status: 'error',
+				message: 'Не удалось отправить письмо'
+			}
+		}
+	}
 }
 
 const confirmReset = async (fd: FormData): Promise<ActionResponse> => {
-    return {
-        status: 'ok',
-        message: 'Вы успешно сменили свой пароль'
-    }
+	return {
+		status: 'ok',
+		message: 'Вы успешно сменили свой пароль'
+	}
 }
 
 const approveEmail = async (fd: FormData): Promise<ActionResponse> => {
-    try {
-        const res = await fetchService.post(`api/account/confirm-email`, {
-            body: JSON.stringify({
-                code: "1111"
-            }),
-        })
-        if(!res.ok) {
-            const message = res.data.detail
-            throw new Error(message)
-        } else {
-            return {
-                status: 'ok',
-                message: 'Вы успешно подтвердили вашу почту'
-            }
-        }
-    } catch (e) {
-        if (e instanceof Error) {
-            return {
-                status: 'error',
-                message: e.message,
-            }
-        }
-        return {
-            status: 'error',
-            message: 'Что-то пошло не так, попробуйте еще раз',
-        }
-    }
+	try {
+		const res = await fetchService.post(`api/account/confirm-email`, {
+			body: JSON.stringify({
+				code: "1111"
+			})
+		})
+		if (!res.ok) {
+			const message = res.data.detail
+			throw new Error(message)
+		} else {
+			return {
+				status: 'ok',
+				message: 'Вы успешно подтвердили вашу почту'
+			}
+		}
+	} catch (e) {
+		if (e instanceof Error) {
+			return {
+				status: 'error',
+				message: e.message
+			}
+		}
+		return {
+			status: 'error',
+			message: 'Что-то пошло не так, попробуйте еще раз'
+		}
+	}
 }
 
 export {
-    getConfiguration,
-    checkEmailExists,
-    loginIntoAccount,
-    registerAccount,
-    resetPassword,
-    approveEmail,
-    successResponse,
-    confirmReset
+	getConfiguration,
+	checkEmailExists,
+	loginIntoAccount,
+	registerAccount,
+	resetPassword,
+	approveEmail,
+	successResponse,
+	confirmReset
 }
