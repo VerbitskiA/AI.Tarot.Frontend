9716e388e39adb87f47bf7cac6dc4d8803e5943c
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-13 13:24:13 +0300

    feat: added DEF_QUESTIONS

---
+++ src/components/widgets/DefaultQuestions/constants.ts
@@ -0,0 +1,76 @@
+const DEF_QUESTIONS = [
+    {
+        id: 1,
+        text: "Does my partner truly love me?"
+    },
+    {
+        id: 2,
+        text: "Will my ex come back to me?"
+    },
+    {
+        id: 3,
+        text: "What energy surrounds me today?"
+    },
+    {
+        id: 4,
+        text: "What should I focus on this week?"
+    },
+    {
+        id: 5,
+        text: "Should I wait for him or move on?"
+    },
+    {
+        id: 6,
+        text: "What are his true feelings for me?"
+    },
+    {
+        id: 7,
+        text: "Is he cheating on me or hiding something?"
+    },
+    {
+        id: 8,
+        text: "Will my financial situation improve soon?"
+    },
+    {
+        id: 9,
+        text: "What should I focus on to achieve success?"
+    },
+    {
+        id: 10,
+        text: "Will I find my soulmate soon?"
+    },
+    {
+        id: 11,
+        text: "Why do I keep attracting the wrong men?"
+    },
+    {
+        id: 12,
+        text: "What is blocking me from having a happy relationship?"
+    },
+    {
+        id: 13,
+        text: "Is this relationship worth my time and energy?"
+    },
+    {
+        id: 14,
+        text: "What will happen between us in the next three months?"
+    },
+    {
+        id: 15,
+        text: "How does he see me as a woman?"
+    },
+    {
+        id: 16,
+        text: "Will I ever get married?"
+    },
+    {
+        id: 17,
+        text: "What can I do to improve my love life?"
+    },
+    {
+        id: 18,
+        text: "What do I need to do to attract true love?"
+    },
+]
+
+export default DEF_QUESTIONS
