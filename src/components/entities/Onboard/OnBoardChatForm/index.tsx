81e4cea393a98db5c1ee29858529bf8f8a8148b9
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-14 14:15:46 +0300

    fix: chat max-height

--- src/components/entities/Onboard/OnBoardChatForm/index.tsx
+++ src/components/entities/Onboard/OnBoardChatForm/index.tsx
@@ -50,12 +50,28 @@ const OnBoardChatForm = () => {
 		console.log("id", id)
 	}, [])
 
+	/*	
+		header 		- 	58px
+		---------------------
+		gap-2 		- 	0.8rem = 8px
+		---------------------
+		qInput		-	78px
+		---------------------
+		custList	-	74px
+		---------------------
+		cScroll		-	8px
+		---------------------
+		cScroll:sm	-	18px
+		---------------------
+		res			=	58px + 2*8 + 78 + 74 + 8px 		= 234px (was 150px (withot customList))
+		res:sm		=	res + (cScroll:sm - cScroll) 	= 244px
+	*/
+
 	return (
 		<>
 			<div className="grid place-items-start h-full">
 				<div className="flex flex-col min-h-[calc(100dvh-58px)] h-full justify-center gap-2 w-full">
-					<div
-						className={`z-10 flex-grow overflow-y-auto max-h-[calc(100dvh-150px)] sm:max-h-[calc(100dvh-150px)]'`}>
+					<div className={`z-10 flex-grow overflow-y-auto max-h-[calc(100dvh-234px)] sm:max-h-[calc(100dvh-244px)]`}>
 						<div className={'flex flex-col w-full gap-6 h-full '}>
 							<div className={'w-full flex flex-col justify-center text-center'}>
 								<ImageBlock imageSrc={'/onboard.png'}/>
