84d8d732b7582b27debc069167d40611abebc65c
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-23 16:14:57 +0300

    fix: AuthLayoutWithAbcoluteHeader

--- src/app/auth/(absoluteHeader)/layout.tsx
+++ src/app/auth/(absoluteHeader)/layout.tsx
@@ -1,25 +1,32 @@
 import React from "react"
 import AppNavBar from "@/components/widgets/AppNavBar"
+import "@/app/auth/(absoluteHeader)/index.css"
 
-const AuthLayoutWithAbsoluteHeader = async ({ children }: { children: React.ReactNode }) => {
+type AuthLayoutWithAbcoluteHeaderProps = {
+    children: React.ReactNode
+}
+
+const AuthLayoutWithAbcoluteHeader = async ({
+    children
+}: AuthLayoutWithAbcoluteHeaderProps) => {
     return (
-        <>
+        <div
+            className={
+                "layoutWithAbsoluteHeader h-[100dvh] w-full bg-gradient-with-image bg-cover bg-center"
+            }
+        >
+            <AppNavBar />
             <div
-                className={
-                    "h-[100dvh] w-full bg-gradient-with-image bg-cover bg-center"
-                }
+                className={`flex h-full w-full items-center justify-center overflow-y-hidden`}
             >
-                <AppNavBar />
-                <div
-                    className={`flex h-[calc(100dvh-var(--header-height))] justify-center`}
+                <main
+                    className={"h-full w-full max-w-[520px] overflow-auto px-2"}
                 >
-                    <main className={"h-full w-full max-w-[520px]"}>
-                        <div className="appContainer h-full">{children}</div>
-                    </main>
-                </div>
+                    {children}
+                </main>
             </div>
-        </>
+        </div>
     )
 }
 
-export default AuthLayoutWithAbsoluteHeader
+export default AuthLayoutWithAbcoluteHeader
