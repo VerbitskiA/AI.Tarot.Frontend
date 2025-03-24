665983b0646ac20690170c6ad3042ae68e091e7a
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-25 00:13:12 +0300

    feat: add api/auth route

---
+++ src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { SessionStrategy } from 'next-auth'
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+        }),
+    ],
+    session: {
+        strategy: 'jwt' as SessionStrategy,
+    },
+    secret: process.env.NEXT_AUTH_SECRET as string
+}
+
+const handler = NextAuth(authOptions)
+export { 
+    handler as GET,
+    handler as POST
+}
