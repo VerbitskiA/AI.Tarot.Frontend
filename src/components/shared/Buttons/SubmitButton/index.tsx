d04b2db3aad6c3f08ed1a83ec94d12085d06e58e
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-25 01:14:12 +0300

    fix: submit button height

--- src/components/shared/Buttons/SubmitButton/index.tsx
+++ src/components/shared/Buttons/SubmitButton/index.tsx
@@ -21,8 +21,8 @@ const SubmitButton: React.FC<SubmitBtn> = ({
 
     return (
         <Button disabled={props.disabled || pending} size={'lg'} color={destructive ? 'danger' : 'primary'}
-                className={cn(`flex items-center gap-2 sticky shadow-button bg-[#27ACC9] data-[hover=true]:bg-[#32cbed] h-[60px] sm:h-[76px] font-semibold text-xl sm:text-2xl rounded-[60px]`, props.className)}
-                type={'submit'}
+            className={cn(`flex items-center gap-2 sticky shadow-button bg-[#27ACC9] data-[hover=true]:bg-[#32cbed] h-[var(--main-submit-button-height)] customMinH769:h-[76px] font-semibold text-xl customMinH769:text-2xl rounded-full`, props.className)}
+            type={'submit'}
         >
             {pending && <ChevronCircleTopLinearIcon className={'animate-spin'}/>}
             {label}
