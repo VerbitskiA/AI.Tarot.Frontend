e9700cea378f1ab0352d781ddb771ff78e2ed4b5
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-24 14:59:42 +0300

    fix: commented useSession

--- src/components/entities/Auth/GoogleBtn/index.tsx
+++ src/components/entities/Auth/GoogleBtn/index.tsx
@@ -5,17 +5,17 @@ import { useSession, signIn } from "next-auth/react"
 export default function GoogleBtn() {
     const btnText = "Continue with Google"
 
-    const { data: session } = useSession()
+    // const { data: session } = useSession()
 
-    if (session) {
-        console.log("user", session.user)
+    // if (session) {
+        // console.log("user", session.user)
         // return (
         //     <>
         //         Signed in as {session.user?.email} <br />
         //         <button onClick={() => signOut()}>Sign out</button>
         //     </>
         // )
-    }
+    // }
 
     const handleClick = () => {
         signIn('google')
