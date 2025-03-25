49a81df989e5e0718937a3f965a9e25a7611046f
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-25 20:01:02 +0300

    fix: authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ import { NextAuthOptions } from 'next-auth'
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string,
+            clientId: process.env.GOOGLE_ID ?? "",
+            clientSecret: process.env.GOOGLE_SECRET ?? "",
         }),
     ],
     session: {
