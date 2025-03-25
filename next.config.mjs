4d9a7f22fb06cd989faa41a8efe9dda5a87bb159
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-25 10:31:45 +0300

    fix: add env config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    env: {
+        GOOGLE_ID: process.env.NEXT_PUBLIC_GOOGLE_ID,
+        GOOGLE_SECRET: process.env.NEXT_PUBLIC_SECRET,
+    }
 };
 
 export default nextConfig;
