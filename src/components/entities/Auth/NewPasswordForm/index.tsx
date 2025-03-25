1940ca4209f4c763053ee1b86ecd02bbe756b0d8
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-25 11:39:55 +0300

    feat: added ImageBlock for new-pass-form

--- src/components/entities/Auth/NewPasswordForm/index.tsx
+++ src/components/entities/Auth/NewPasswordForm/index.tsx
@@ -4,6 +4,9 @@ import PasswordField from "@/components/shared/Inputs/PasswordField";
 import FormWrapper from "@/components/shared/FormWrapper";
 import {Image} from "@nextui-org/image";
 import {ActionResponse} from "@/configs/http-service/fetch-settings/types";
+import ImageBlock from '../ImageBlock';
+import { getAvatarSize, isMinHeight1024MediaQuery, isMinHeight768MediaQuery } from '@/components/shared/helpers';
+import { useMediaQuery } from 'react-responsive';
 
 type Props = {
     handleResetPassword: ((fd: FormData) => Promise<ActionResponse>)
@@ -12,6 +15,9 @@ type Props = {
 const NewPasswordForm: FC<Props> = ({handleResetPassword}) => {
     const [newPasswordValue, setNewPasswordValue] = useState("");
     const [repeatPasswordValue, setRepeatPasswordValue] = useState("");
+
+    const isMinHeight768 = useMediaQuery(isMinHeight768MediaQuery)
+    const isMinHeight1024 = useMediaQuery(isMinHeight1024MediaQuery)
 
     const isInvalid = useMemo(() => {
         if (newPasswordValue === "" || repeatPasswordValue === "") {
@@ -29,10 +35,24 @@ const NewPasswordForm: FC<Props> = ({handleResetPassword}) => {
                              actionLabel={'Reset password'}>
                     <div className={'flex flex-col w-full gap-6 h-full '}>
                         <div className={'w-full flex flex-col gap-2 justify-center text-center items-center'}>
-                            <Image src={'/authImage.jpg'} alt={'logo'} width={200} height={200}/>
+                            <ImageBlock
+                                imageSrc={'/authImage.jpg'}
+                                avatarSize={getAvatarSize(
+                                    [
+                                        {value: isMinHeight1024, size: "large"},
+                                        {value: isMinHeight768, size: "medium"},
+                                    ],
+                                    "small"
+                                )}
+                            >
+                                <h1 className={'w-full text-center text-2xl font-bold'}>
+                                    Create new password
+                                </h1>
+                            </ImageBlock>
+                            {/* <Image src={'/authImage.jpg'} alt={'logo'} width={200} height={200}/>
                             <h1 className={'w-full text-center text-2xl font-bold'}>
                                 Create new password
-                            </h1>
+                            </h1> */}
                         </div>
                         <div className={'flex flex-col gap-2'}>
                             <PasswordField
