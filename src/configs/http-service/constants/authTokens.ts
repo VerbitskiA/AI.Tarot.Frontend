f50bc7b6c01bb2334d0d90db386c97f52bbd1fcb
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-26 23:50:26 +0300

    feat: changed TOKENS_KEYS

--- src/configs/http-service/constants/authTokens.ts
+++ src/configs/http-service/constants/authTokens.ts
@@ -1,5 +1,9 @@
 const AUTH_ACCESS_TOKEN = 'ai-tarot-id';
 
 export const TOKENS_KEYS = {
+    // TODO: delete "access"
     access: AUTH_ACCESS_TOKEN,
+
+    ACCESS_TOKEN: "accessToken",
+    REFRESH_TOKEN: "refreshToken",
 }
