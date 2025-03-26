1b9d4689231394d8ba5a6b2d80f64718aaa6c49a
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-27 00:08:06 +0300

    refactor: fetch-settings types, changed FetchOptionsT

--- src/configs/http-service/fetch-settings/types/index.ts
+++ src/configs/http-service/fetch-settings/types/index.ts
@@ -1,46 +1,52 @@
-type FetchMethodT = 'get' | 'post' | 'put' | 'patch' | 'delete'
+type FetchMethodT = "get" | "post" | "put" | "patch" | "delete"
 export type FetchQueryParamsT = {
     [key: string]: string | number
 }
 export type FetchOptionsT = {
-    params?: FetchQueryParamsT,
-    source?: 'server' | 'client'
-} & Omit<RequestInit, 'method'>
+    params?: FetchQueryParamsT
+    source?: "server" | "client"
+    tokens?: {
+        accessToken: string
+        refreshToken: string
+    }
+} & Omit<RequestInit, "method">
 
 export type SuccessFetchResponse<ReturnType = unknown> = {
-    status: number,
-    headers: Headers,
-    ok: true,
+    status: number
+    headers: Headers
+    ok: true
     data: ReturnType
 }
 export type ErrorFetchResponse = {
-    status: number,
-    headers: Headers,
-    ok: false,
-    data: {detail: string}
+    status: number
+    headers: Headers
+    ok: false
+    data: { detail: string }
 }
-type FetchResponseT<ReturnType = unknown> = SuccessFetchResponse<ReturnType> | ErrorFetchResponse
+type FetchResponseT<ReturnType = unknown> =
+    | SuccessFetchResponse<ReturnType>
+    | ErrorFetchResponse
 
-type SuccessActionResponse = {
-    status: 'ok',
+export type SuccessActionResponse = {
+    status: "ok"
     message?: string
 }
-type RejectActionResponse = {
-    status: 'error',
+export type RejectActionResponse = {
+    status: "error"
     message: string
 }
 export type ActionResponse = SuccessActionResponse | RejectActionResponse
 
 export type PaginatedFetchResponse<Data> = {
-    previous: string,
-    next: string,
-    count: number,
-    results: Data[],
+    previous: string
+    next: string
+    count: number
+    results: Data[]
 }
 
-export type FetchServiceT= {
+export type FetchServiceT = {
     [key in FetchMethodT]: <ReturnType = unknown>(
         url: string,
-        options?: FetchOptionsT,
+        options?: FetchOptionsT
     ) => Promise<FetchResponseT<ReturnType>>
 }
