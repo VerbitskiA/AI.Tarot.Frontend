b3409d4a40f3e4a551b406952c631767583b54c0
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-28 19:36:19 +0300

    fix: user and jwt types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,17 +1,21 @@
 import NextAuth from "next-auth"
+import { TokensData } from "@/lib/types/responsesData"
+
+type Provider = "credentials" | "google"
 
 declare module "next-auth" {
     /**
      * Returned by `useSession`, `getSession` and received as a prop on the `Provider` React Context
      */
     interface User {
-        // id is required on DefaultUser type, userId = id
-        id: string
-        tokens: {
-            accessToken: string
-            refreshToken: string
-        }
-        userInfo: {
+        // DefaultUser:
+        // id: string
+        // name?: string | null
+        // email?: string | null
+        // image?: string | null
+        
+        tokens?: TokensData
+        userInfo?: {
             userId: string
             username: string
             balance: number
@@ -24,18 +28,20 @@ declare module "next-auth" {
 
 declare module "next-auth/jwt" {
     interface JWT {
-        user: {
+        user?: {
             // id is required on DefaultUser type, userId = id
             id: string
-            tokens: {
-                accessToken: string
-                refreshToken: string
-            }
-            userInfo: {
+            name?: string | null
+            email?: string | null
+            image?: string | null
+            
+            tokens?: TokensData
+            userInfo?: {
                 userId: string
                 username: string
                 balance: number
             }
-        }
+        },
+        provider?: Provider | string
     }
 }
