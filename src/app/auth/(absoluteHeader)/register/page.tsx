390f576c893b000262eccbc10d3a3428e7b27adb
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2025-03-31 14:16:51 +0300

    fix: handleCheckEmail

--- src/app/auth/(absoluteHeader)/register/page.tsx
+++ src/app/auth/(absoluteHeader)/register/page.tsx
@@ -1,5 +1,3 @@
-'use server'
-
 import {checkIsEmailExist} from '@/lib/serverActions/auth'
 import React, {FC} from 'react'
 import {redirect} from "next/navigation";
@@ -17,43 +15,25 @@ const Page: FC<Props> = async({searchParams}) => {
     const onboardQuestion = !!searchParams?.onboardQuestion
     const handleCheckEmail = async (fd: FormData): Promise<ActionResponse> => {
         'use server'
+
         const email = fd.get("email")
 
         if (email && typeof email === "string") {
-            try {
-                const res = await checkIsEmailExist(email)
+			const res = await checkIsEmailExist(email)
 
-                console.log("checkIsEmailExist", res)
+			if (res === false) {
+				onboardQuestion ?
+					redirect(`/auth/register?addInfo=true&onboardQuestion=${onboardQuestion}`)
+					:
+					redirect(`/auth/register?addInfo=true`)
+			}
 
-                if (res) {
-                    onboardQuestion ?
-                        redirect(`/auth/register?addInfo=true&onboardQuestion=${onboardQuestion}`)
-                        :
-                        redirect(`/auth/register?addInfo=true`)
-                    
-                    return {
-                        status: "ok",
-                    }
-                }
-
-                return {
-                    status: "error",
-                    message: "Email is exist"
-                }
-                
-            } catch (error: unknown) {
-                if (error instanceof Error) {
-                    return {
-                        status: "error",
-                        message: error.message
-                    }
-                }
-                return {
-                    status: "error",
-                    message: "Something went wrong with handleCheckEmail"
-                }
-            }
+			return {
+				status: "error",
+				message: "Email is exist"
+			}
         }
+
         return {
             status: "error",
             message: "Not valid email"
